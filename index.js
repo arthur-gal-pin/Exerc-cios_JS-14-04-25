3311833babc464fed99431e2896ba205adc0271c
unknown <arthurrgspinto@gmail.com>
2025-04-14 14:41:42 -0300

    Primeiro exercício completo

---
+++ index.js
@@ -0,0 +1,11 @@
+//Exercício 1:Crie um programa que peça um número ao usuário e exiba na tela todos os números de 1 até o número digitado.
+const num = parseInt(prompt('Insira um número:'));
+if (isNaN(num) || num <= 1) {
+    alert("Você colocou um valor inválido para número.")
+} else {
+    let frase = "";
+    for (let i = 1; i <= num; i++) {
+        frase = frase + " " + i;
+    }
+    alert(frase);
+}
