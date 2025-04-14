766aecc40ed52f05808de753a0239aa38425ab2c
unknown <arthurrgspinto@gmail.com>
2025-04-14 16:19:04 -0300

    Exercício 5 feito. Falta corrigir o caso de números primos.

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-//Exercício 4: Crie um programa que solicite 5 notas e calcule a média delas.
-alert("Insira suas notas:");
-let soma = 0;
-let num = 0;
+//Exercício 4: Peça dois números ao usuário e exiba todos os números pares no intervalo entre eles. Exemplo: O usuário digita 11 e 20, seu programa deve apresentar os números 12, 14, 16, 18 e 20.
+const numeroMe = parseInt(prompt("Insira o menor número:"));
+const numeroMa = parseInt(prompt("Insira o maior número:"));
+sequencia = "";
 
-for (let i = 1; i <= 5; i++) {
-
-    num = parseFloat(prompt(`Nota da prova ${i}:`)); // Solicita as notas ao usuário
-
-    if (isNaN(num) || num < 0 || num > 10) {
-        alert("Valor inválido.");
+if (!isNaN(numeroMa) && !isNaN(numeroMa) && numeroMe < numeroMa && numeroMa > 0 && numeroMe >= 0) {
+    if (numeroMe % 2 != 0) {
+        numeroMe = numeroMe + 1;
+        for (let i = numeroMe; i <= numeroMa; i += 2) {
+            sequencia += i + " ";
+        }
     } else {
-        soma += num; // Adiciona o número à soma
+        for (let i = numeroMe; i <= numeroMa; i += 2) {
+            sequencia += i + " ";
+        }
     }
+    alert(sequencia);
+} else {
+    alert("Foi inserido um valor inválido.");
 }
-const media = soma / 5
-alert("A sua média alcançada foi de: " + media.toFixed(2) + " pontos."); // Exibe a média final
+
 
 
