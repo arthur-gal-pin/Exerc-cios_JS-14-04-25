4d1574ecc34ab27076bff71a71ab9701e683f9ed
unknown <arthurrgspinto@gmail.com>
2025-04-14 15:39:56 -0300

    Exercício Quatro Funcional

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-//Exercício 2: Peça para o usuário digitar 10 números inteiros e exiba a soma deles no final.
-alert("Insira 10 números para soma.");
+//Exercício 4: Crie um programa que solicite 5 notas e calcule a média delas.
+alert("Insira suas notas:");
 let soma = 0;
+let num = 0;
 
-for (let i = 1; i <= 10; i++) {
-    let num = parseInt(prompt(`Insira o número ${i}:`)); // Solicita o número ao usuário
-    if (isNaN(num) || num <= 1) {
+for (let i = 1; i <= 5; i++) {
+
+    num = parseFloat(prompt(`Nota da prova ${i}:`)); // Solicita as notas ao usuário
+
+    if (isNaN(num) || num < 0 || num > 10) {
         alert("Valor inválido.");
-    } else{
+    } else {
         soma += num; // Adiciona o número à soma
     }
 }
-
-alert("Soma: " + soma); // Exibe a soma final
+const media = soma / 5
+alert("A sua média alcançada foi de: " + media.toFixed(2) + " pontos."); // Exibe a média final
 
 
