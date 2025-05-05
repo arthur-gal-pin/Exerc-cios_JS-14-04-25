c1bd46076034c8eacc129e4e23bf96ccd80982ce
unknown <arthurrgspinto@gmail.com>
2025-05-05 12:27:30 -0300

    n

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 //Exercício 4: Peça dois números ao usuário e exiba todos os números pares no intervalo entre eles. Exemplo: O usuário digita 11 e 20, seu programa deve apresentar os números 12, 14, 16, 18 e 20.
 const numeroMe = parseInt(prompt("Insira o menor número:"));
 const numeroMa = parseInt(prompt("Insira o maior número:"));
-sequencia = "";
+let sequencia = "";
 
-if (!isNaN(numeroMa) && !isNaN(numeroMa) && numeroMe < numeroMa && numeroMa > 0 && numeroMe >= 0) {
-    if (numeroMe % 2 != 0) {
-        numeroMe = numeroMe + 1;
-        for (let i = numeroMe; i <= numeroMa; i += 2) {
-            sequencia += i + " ";
-        }
-    } else {
-        for (let i = numeroMe; i <= numeroMa; i += 2) {
-            sequencia += i + " ";
-        }
+if (!isNaN(numeroMe) && !isNaN(numeroMa) && numeroMe < numeroMa && numeroMa > 0 && numeroMe >= 0) {
+    // Ajusta numeroMe para o próximo par se for ímpar
+    if (numeroMe % 2 !== 0) {
+        numeroMe += 1;
     }
+
+    // Loop para adicionar os números pares à sequência
+    for (let i = numeroMe; i <= numeroMa; i += 2) {
+        sequencia += i + " ";
+    }
+
     alert(sequencia);
 } else {
     alert("Foi inserido um valor inválido.");
 }
+
 
 
 
